174892c936733dd93babaed87524f7bfb8096718
Uttkarsh Pawan <utkbest@gmail.com>
2025-11-22 20:12:39 +0530

    feat: add qap representation

--- src/qap.rs
+++ src/qap.rs
@@ -7,12 +7,17 @@ use r1cs::{LEFT_MATRIX, RIGHT_MATRIX, RESULT_MATRIX};
 use witness::WITNESS;
 
 pub fn qap_representation(matrix: &[[Fr; 3]; 2], witness: &[Fr; 3]) -> [Fr; 2] {
-    let un_x: [[Fr; 2]; 3] = interpolate_matrix(matrix);
-    let mut a_un_x: [Fr; 2] = scalar_mul(un_x[0], witness[0]);
+    let matn_x: [[Fr; 2]; 3] = interpolate_matrix(matrix);
+    let mut a_matn_x: [Fr; 2] = scalar_mul(matn_x[0], witness[0]);
 
     for i in 1..3 {
-        a_un_x = add_2(a_un_x, scalar_mul(un_x[i], witness[i]));
+        a_matn_x = add_2(a_matn_x, scalar_mul(matn_x[i], witness[i]));
     }
 
-    a_un_x
+    a_matn_x
 }
+
+pub static u_x = qap_representation(&LEFT_MATRIX, &WITNESS);
+pub static v_x = qap_representation(&RIGHT_MATRIX, &WITNESS);
+pub static w_x = qap_representation(&RESULT_MATRIX, &WITNESS);
+
